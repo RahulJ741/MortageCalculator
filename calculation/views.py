608f0537cfb805d2421f81b0236e877609d29d60
RahulJ <rahulj9432@gmail.com>
2018-09-27 16:01:56 +0530

    corrected the intrest saved parameter

--- calculation/views.py
+++ calculation/views.py
@@ -73,7 +73,7 @@ def return_ammotization(loan_amount,old_roi, new_roi, old_months, paid_months, c
         print (remaining_principle, "Remaining principle")
         if case_opt == 1:
             emi = round(abs(np.pmt(new_emi_rate, old_months-paid_months, remaining_principle)),0)
-            new_intrest_total = abs((abs(emi)*old_months-paid_months) - remaining_principle)
+            new_intrest_total = abs((abs(emi)*(old_months-paid_months)) - remaining_principle)
             intrest_saved = old_intrest_total - new_intrest_total
             print (emi ,"EMI in case 1",new_intrest_total, "::::::", intrest_saved)
             return_list = get_annotized_list(new_emi_rate, remaining_principle, emi, old_months-paid_months)
